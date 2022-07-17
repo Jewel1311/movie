0d2c484ee4ed945fbd1a8a7f229f90cf0eb314f1
Jewel1311 <jewelmathew13@gmail.com>
2022-07-17 09:32:18 +0530

    Movies search

---
+++ movies/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from .settings import API_KEY
+import json
+import requests
+from django.contrib import messages
+
+
+def search_movie(request):
+    if request.method == "POST":
+        name = request.POST['movie']
+        if name is not None:
+            return redirect('movie_view', name=name)
+    return render(request, 'movies/index.html')
+
+def movie_view(request, name):
+    details = requests.get(f"https://imdb-api.com/API/SearchMovie/{API_KEY}/{name}").json()
+    count = len(details['results'])
+    if count != 0:
+        context = {
+            'results': details['results']
+        } 
+        return render(request, 'movies/list.html',context)
+    messages.warning(request,f'No movies found')
+    return render(request, 'movies/list.html')
+ 
+
+def detail_view(request, id):
+    return redirect(f"https://www.imdb.com/title/{id}/")
